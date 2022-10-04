63e195d54e3b5c099945b21591ead4e7dd78ccb2
YayangSB <yayang.setia.tif420@polban.ac.id>
2022-10-04 23:34:00 +0700

    Cek bilangan ganjil genap

--- Latihan/Soal3.hs
+++ Latihan/Soal3.hs
@@ -1,6 +1,6 @@
 checkEvenOddPosNeg :: Integer -> String
 checkEvenOddPosNeg (x) 
-    | (mod x 2) == 0 && x<0 = "Genap Negatif"
-    | (mod x 2) == 0 && x>0 = "Genap Positif"
-    | (mod x 2) == 1 && x<0 = "Ganjil Negatif"
-    | otherwise = "Ganjil Positif"
+    | (mod x 2) == 0 && x<0 = "Genap Negatif"  -- jika bilangannya genap dan negatif
+    | (mod x 2) == 0 && x>0 = "Genap Positif"  -- jika bilangannya genap dan positif
+    | (mod x 2) == 1 && x<0 = "Ganjil Negatif" -- jika bilangannya ganjil dan negatif
+    | otherwise = "Ganjil Positif"             -- jika bilangannya ganjil dan positif
