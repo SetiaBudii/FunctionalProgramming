1dd3dc7e8d0a0cf3fc1001c2b9ad91289b5d6e21
YayangSB <yayang.setia.tif420@polban.ac.id>
2022-10-04 23:29:10 +0700

    List bilangan prima dengan List Comprehensions

--- Latihan/Soal5.hs
+++ Latihan/Soal5.hs
@@ -1,12 +1,15 @@
+{- Function untuk cek bilangan prima dengan 1 input dengan menggunakan bantuan function cekprime-}
 cekPrime :: Integer -> Bool
 cekPrime 1 = False
 cekPrime 2 = True
 cekPrime n = cekprime n n
 
+{- Function untuk cek bilangan prima dengan 2 input secara rekursive sampai b < 2 -}
 cekprime :: Integer-> Integer -> Bool
 cekprime a b = if (b < 2 ) then True else 
                 if (mod a b == 0 && a /= b) then False else cekprime a (b-1)
 
+{- Function untuk mencetak bilangan prima sampai dengan n bilangan -}
 listprime :: Integer -> [Integer]
 listprime n = 
         [x| x <-[2..n], cekPrime x]
