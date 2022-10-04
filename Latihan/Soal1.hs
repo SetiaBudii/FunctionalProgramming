0ed3a43875a155c7ea516f36cab36e86038251f4
YayangSB <yayang.setia.tif420@polban.ac.id>
2022-10-04 21:31:16 +0700

    Konversi Nilai

---
+++ Latihan/Soal1.hs
@@ -0,0 +1,9 @@
+cek_Nilai :: Integer -> String
+cek_Nilai x
+    | x >= 80 = "A" 
+    | x >= 75 = "AB" 
+    | x >= 70 = "B" 
+    | x >= 65 = "BC" 
+    | x >= 60 = "C"
+    | x >= 50 = "D"
+    | otherwise = "E"  
