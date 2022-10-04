e5f3631418eca639f0217cdf7f415fe34ca94da0
YayangSB <yayang.setia.tif420@polban.ac.id>
2022-10-04 23:36:37 +0700

    Pembagi bilangan terbesar (algoritma euclidiance)

--- Latihan/Soal2.hs
+++ Latihan/Soal2.hs
@@ -1,5 +1,5 @@
 gcde :: (Integer,Integer) -> Integer
 gcde(x,y)
-    | (x == y) =   y
-    | (x > y ) = gcde((x-y),y)
-    | otherwise = gcde((y-x),x)
+    | (x == y) =   y            -- jika x sama dengan y
+    | (x > y ) = gcde((x-y),y)  -- jika x > y
+    | otherwise = gcde((y-x),x) -- jika x < y
