b8d70306034bdad3d5cb137c0015225cc5e7d451
YayangSB <yayang.setia.tif420@polban.ac.id>
2022-10-04 21:32:01 +0700

    algoritma euclidiance

--- Minggu 6/Soal1.hs
+++
@@ -1,9 +0,0 @@
-cek_Nilai :: Integer -> String
-cek_Nilai x
-    | x >= 80 = "A" 
-    | x >= 75 = "AB" 
-    | x >= 70 = "B" 
-    | x >= 65 = "BC" 
-    | x >= 60 = "C"
-    | x >= 50 = "D"
-    | otherwise = "E"  
