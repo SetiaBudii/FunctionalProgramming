3d551f1914c0c87c0cbea5d7a7dc290e4f2b6e72
YayangSB <yayang.setia.tif420@polban.ac.id>
2022-10-08 15:59:21 +0700

    Membuat bentuk Bintang (OpenGL & Haskell)

---
+++ OpenGl/Bintang.hs
@@ -0,0 +1,36 @@
+import Graphics.UI.GLUT
+import Graphics.Rendering.OpenGL
+
+renderInWindow displayFunction = do
+    (progName,_) <- getArgsAndInitialize
+    createWindow progName
+    displayCallback $= displayFunction
+    mainLoop
+
+displayPoints points primitiveShape = do
+    renderAs primitiveShape points
+    flush
+
+renderAs figure ps = renderPrimitive figure$makeVertexes ps
+
+makeVertexes = mapM_ (\(x,y,z)->vertex$Vertex3 x y z)
+
+mainFor primitiveShape = renderInWindow (displayMyPoints primitiveShape)
+
+displayMyPoints primitiveShape = do
+    clear [ColorBuffer]
+    currentColor $= Color4 1 1 0 0
+    displayPoints myPoints primitiveShape
+
+myPoints
+  = [(0,0.5,0::GLfloat)
+    ,(3.5/6,-3.5/6,0)
+    ,(-4.3/6,0.9/6,0)
+    ,(4.3/6,0.9/6,0)
+    ,(-3.5/6,-3.5/6,0)
+    ]
+
+main = mainFor LineLoop
+{--menghubungkan antar point (membuat line) dengan cara menghubungkan titik ke titik secara countinue (melanjutkan
+   dari titik sebelumnya)--}
+      
